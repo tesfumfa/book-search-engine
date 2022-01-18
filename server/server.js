ef0407c9d5739244563ec261a65f66fe7b6e14b5
tesfumfa <33948864+tesfumfa@users.noreply.github.com>
2022-01-17 22:46:10 -0500

    update server

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const db = require("./config/connection");
 // const routes = require("./routes");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3052;
 
 const server = new ApolloServer({
   typeDefs,
